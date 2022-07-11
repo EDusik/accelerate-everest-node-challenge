462ee8ed28f618471321669f33dee04fe9c15cf3
EDusik <eduardodusik@gmail.com>
2022-07-11 16:40:36 -0300

    feat: add customer js type

---
+++ challenge-01/src/domain/types/customer.js
@@ -0,0 +1,15 @@
+export class Customer {
+	full_name;
+	email;
+	email_confirmation;
+	cpf;
+	cellphone;
+	birth_;
+	email_sms;
+	whats_app;
+	country;
+	city;
+	postal_code;
+	address;
+	number;
+}
