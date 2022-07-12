5fc4ffa05a8a665701b7e3ee0384812bf61f3e63
EDusik <eduardodusik@gmail.com>
2022-07-12 14:35:04 -0300

    refactor: update customerSchema

--- challenge-01/src/presentation/schemas/customer.schema.js
+++ challenge-01/src/presentation/schemas/customer.schema.js
@@ -2,6 +2,18 @@ const Joi = require("joi");
 
 const customerSchema = Joi.object({
 	full_name: Joi.string().required(),
+	email: Joi.string().required(),
+	email_confirmation: Joi.string().valid(Joi.ref("email")).required(),
+	cpf: Joi.string().required(),
+	cellphone: Joi.string().required(),
+	birth_date: Joi.date().required(),
+	email_sms: Joi.boolean().required(),
+	whats_app: Joi.boolean().required(),
+	country: Joi.string().required(),
+	city: Joi.string().required(),
+	postal_code: Joi.string().required(),
+	address: Joi.string().required(),
+	number: Joi.number().required(),
 });
 
 module.exports = customerSchema;
