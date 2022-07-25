7dc211c9b0e91aa3ae13314507198706b497f308
EDusik <eduardodusik@gmail.com>
2022-07-25 17:52:42 -0300

    chore: add helmet lib

--- challenge-01/src/index.js
+++ challenge-01/src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const helmet = require("helmet");
 const routes = require("./presentation/Routes");
 
+const PORT = 3000;
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
